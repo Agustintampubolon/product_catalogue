20c280438b315b05f7844eae0e6c1c5273702918
agustintampubolon <agustintampubolon27@gmail.com>
2022-04-18 13:34:45 +0700

    add logo

--- components/Header.vue
+++ components/Header.vue
@@ -5,6 +5,7 @@
         class="px-4 sm:px-6 lg:px-8 border-b border-ui-medium flex items-center justify-between"
       >
         <div class="flex items-center">
+          <logo />
           <div class="hidden lg:flex lg:items-center">
             <div
               class="hidden flex-grow items-center justify-center lg:flex text-sm font-medium"
---
+++ components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-16 flex items-center">
+    <div class="ml-4 flex lg:ml-0 lg:mr-8">
+      <nuxt-link to="/">
+        <img style="width: 50px" src="~/static/logo-fix.png" alt="Balanjuo" />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo",
+};
+</script>
